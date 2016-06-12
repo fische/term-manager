a48d5585a72a6e00fce969975c283f68a33e3583
Maxime Fischer <maxime2fischer@gmail.com>
2016-06-12 14:28:34 +0100

    Set up .eslintrc for react and flow and change gulpfile according to new eslintrc.

--- gulpfile.js
+++ gulpfile.js
@@ -5,54 +5,55 @@ var flow = require('gulp-flowtype');
 var notify = require('gulp-notify');
 var changed = require('gulp-changed');
 var clean = require('gulp-clean');
+var todo = require('gulp-todo');
+
+gulp.task('todo', function() {
+  gulp.src('src/**/*.js')
+    .pipe(todo())
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('clean', function() {
-  return gulp.src('lib', { read: false })
-          .pipe(clean({ force: true }));
+  return gulp.src('lib', {
+    read: false
+  })
+    .pipe(clean({
+      force: true
+    }));
 });
 
 gulp.task('hint', function() {
-  return gulp.src(['src/**/*.js'], { write: false })
-             .pipe(eslint({
-               configFile: '.eslintrc',
-               useEslintrc: true
-             }))
-             .pipe(eslint.format())
-             .pipe(eslint.failAfterError())
-             .on('error', notify.onError('<%= error.message %>'));
-});
-
-gulp.task('flow', ['hint'], function() {
-  return gulp.src(['src/**/*.js'], { write: false })
-             .pipe(flow({
-               all: false,
-               weak: false,
-               killFlow: false,
-               beep: false,
-               abort: false
-             }))
-             .on('error', notify.onError('<%= error.message %>'));
+  return gulp.src(['src/**/*.js'], {
+    write: false
+  })
+    .pipe(eslint({
+      configFile: '.eslintrc',
+      useEslintrc: true
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('build-dev', ['hint'], function() {
   return gulp.src(['src/**/*.js'])
-             .pipe(changed('lib'))
-             .pipe(babel({
-               sourceMaps: "inline"
-             }))
-             .pipe(gulp.dest('lib'));
+    .pipe(changed('lib'))
+    .pipe(babel({
+      sourceMaps: "inline"
+    }))
+    .pipe(gulp.dest('lib'));
 });
 
-gulp.task('build-prod', ['hint'], function() {
+gulp.task('build-prod', ['hint', 'clean', 'todo'], function() {
   return gulp.src(['src/**/*.js'])
-             .pipe(babel({
-               sourceMaps: false,
-               compact: true
-             }))
-             .pipe(gulp.dest('lib'));
+    .pipe(babel({
+      sourceMaps: false,
+      compact: true
+    }))
+    .pipe(gulp.dest('lib'))
+    .on('error', notify.onError('<%= error.message %>'));
 });
 
-gulp.task('watch', ['build-dev'], function() {
+gulp.task('watch', function() {
   return gulp.watch(['src/**/*.js'], ['build-dev']);
 });
 
