41ba0343a2f679cb0b050bb7391565a537d7a061
Maxime Fischer <maxime2fischer@gmail.com>
2016-07-06 18:42:01 +0100

    Differentiate printable and non-printable char.

--- src/components/Stdin/Stdin.js
+++ src/components/Stdin/Stdin.js
@@ -1,5 +1,11 @@
 import { React } from 'react-for-atom'
 
+const unicode = /U\+[0-9A-F]{4}/;
+
+function checkUnicodeIdentifier(id: string): bool {
+  return unicode.test(id);
+}
+
 export class Stdin extends React.Component {
   constructor(props: object) {
     super(props);
@@ -13,11 +19,19 @@ export class Stdin extends React.Component {
     this.refs.stdin.blur();
   }
 
+  onKeyPress(e) {
+    console.log("keypress", e);
+  }
+
   onKeyDown(e) {
-    console.log(atom.keymaps.keystrokeForKeyboardEvent(e));
+    if (e.keyCode < 32 || !(checkUnicodeIdentifier(e.keyIdentifier))) {
+      console.log("keydown", e);
+      e.preventDefault();
+    }
   }
 
   componentDidMount() {
+    this.refs.stdin.addEventListener('keypress', this.onKeyPress);
     this.refs.stdin.addEventListener('keydown', this.onKeyDown);
   }
 
